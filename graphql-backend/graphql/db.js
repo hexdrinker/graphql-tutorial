e66a7ada5930813578a4bfa3953c7d41af5e2037
박영호 <mellonia.bluerose@gmail.com>
2021-04-10 14:39:26 +0900

    graphql setup with yts movie api

--- graphql-backend/graphql/db.js
+++ graphql-backend/graphql/db.js
@@ -1,60 +1,8 @@
-export let movies = [
-    {
-        id: 1,
-        name: "Inception",
-        director: "Christopher Nolan",
-        score: 9.60
-    },
-    {
-        id: 2,
-        name: "The Dark Knight",
-        director: "Christopher Nolan",
-        score: 9.65
-    },
-    {
-        id: 3,
-        name: "PARASITE",
-        director: "Bong Jun Ho",
-        score: 9.07
-    },
-    {
-        id: 4,
-        name: "Pieta",
-        director: "Kim Ki Deok",
-        score: 7.79
-    },
-    {
-        id: 5,
-        name: "Avengers: End Game",
-        director: "Anthony Russo",
-        score: 9.49
-    }
-]
+import fetch from 'node-fetch';
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getMovieById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-}
-
-export const addMovie = (name, director, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        director,
-        score,
-    }
-    movies.push(newMovie);
-    return newMovie;
+export const getMovies = (limit, rating) => {
+    return fetch(`${API_URL}`)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 }
--- graphql-backend/graphql/resolvers.js
+++ graphql-backend/graphql/resolvers.js
@@ -1,12 +1,8 @@
-import { getMovies, getMovieById, addMovie } from './db';
+import { getMovies } from './db';
 
 const resolvers = {
     Query: {
         movies: () => getMovies(),
-        movie: (_, { id }) => getMovieById(id)
-    },
-    Mutation: {
-        addMovie: (_, { name, director, score }) => addMovie(name, director, score)
     }
 }
 
